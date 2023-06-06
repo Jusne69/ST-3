b5d95ed3994143745b3362f538ae8068c3eae37a
jusne788 <22714513-jusne788@users.noreply.replit.com>
2023-06-06 22:23:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#GUI_canvas2
+from tkinter import *
+class window_one:
+    def __init__(self):
+        Button(main_window,text="Quit",command=self.quit) .grid()
+        Button(main_window.text="Oval",command=self.oval) .grid()
+        Button(main_window,text="Rectngle",command=self.rectangle) .grid()
+      
